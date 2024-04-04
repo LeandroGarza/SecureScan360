de650d2fa1501c9db712a1a514b44af753ddb4c0
LeandroGarza <leandrogarzap@gmail.com>
2024-04-04 11:17:38 -0300

    se puede escanear mas de 1 dominio

--- PortScanner/PortScanner.py
+++ PortScanner/PortScanner.py
@@ -1,18 +1,36 @@
 import socket       #permite establecer la conexion por internet
 from IPy import IP
 
+def scan(target):
+    converted_ip = check_ip(target)
+    print('\n' + '[-_0 Scanning target] ' + str(target))
+    for port in range(1,100):
+        scan_port(converted_ip, port)
+
+def check_ip(ip):
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
 def scan_port(ipaddress, port):
     try:
-     sock = socket.socket()
-     sock.settimeout(0.5)   #cambiarlo a mi gusto
-     sock.connect((ipaddress, port))
-     print('[+] El puerto '+ str(port) + ' esta abierto')
+        sock = socket.socket()
+        sock.settimeout(0.5)   #cambiarlo a mi gusto
+        sock.connect((ipaddress, port))
+        print('[+] El puerto '+ str(port) + ' esta abierto')
     
     except:
-     print('[-] El puerto '+ str(port) + ' esta cerrado')
+        pass
+        # print('[-] El puerto '+ str(port) + ' esta cerrado')
      
     
-ipaddress =  input('[+] Escriba la ip a escanear: ')
+targets =  input('Escriba el dominio o ips a escanear (separados por coma): ')
+if ',' in targets:
+    for ip_add in targets.split(','):
+        scan(ip_add.strip(' '))
+        
+else:
+    scan(targets)
  
-for port in range(1,10):
-    scan_port(ipaddress, port)
