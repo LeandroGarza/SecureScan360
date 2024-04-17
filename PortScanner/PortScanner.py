43750c80b21292c0b001168836e440aa47b53174
LeandroGarza <leandrogarzap@gmail.com>
2024-04-17 09:50:12 -0300

    se agrega version de servicio en c/ puerto

--- PortScanner/PortScanner.py
+++ PortScanner/PortScanner.py
@@ -1,3 +1,5 @@
+# version con clases
+"""
 import socket       #permite establecer la conexion por internet
 from IPy import IP
 
@@ -40,7 +42,7 @@ class PortScan():
             pass
             # print('[-] El puerto '+ str(port) + ' esta cerrado')
      
-"""   
+
     if __name__ == "__main__":      #por si lo ejecutamos de otro archivo
         targets =  input('Escriba el dominio o ips a escanear (separados por coma): ')
         # port_num = input('Ingrese la cantidad de puertos que quiere scanear: ')
@@ -52,4 +54,48 @@ class PortScan():
         else:
             #scan(targets,port_num)
             scan(targets)
+
 """
+# version que indica que servicio corre en cada puerto
+import socket       #permite establecer la conexion por internet
+from IPy import IP
+
+def scan(target):
+    converted_ip = check_ip(target)
+    print('\n' + '[-_0 Scanning target] ' + str(target))
+    for port in range(1,100):
+        scan_port(converted_ip, port)
+
+def check_ip(ip):
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+    
+    
+def get_banner(s):
+    return s.recv(1024)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)   #cambiarlo a mi gusto o incluso sacarlo
+        sock.connect((ipaddress, port))
+        try:
+            banner = get_banner(sock)
+            print('[+] Puerto abierto '+ str(port) + ' : ' + str(banner.decode().strip('\n')))
+        except:
+            print('[+] Puerto abierto '+ str(port))
+    except:
+        pass
+        # print('[-] El puerto '+ str(port) + ' esta cerrado')
+     
+    
+targets =  input('Escriba el dominio o ips a escanear (separados por coma): ')
+if ',' in targets:
+    for ip_add in targets.split(','):
+        scan(ip_add.strip(' '))
+        
+else:
+    scan(targets)
--- PortScanner/VulnScanner.py
+++ PortScanner/VulnScanner.py
@@ -1,12 +1,13 @@
+"""
 import PortScanner
 
 # print('[+] Puerto abierto '+ str(port) + ' : ' + str(banner.decode().strip('\n')))
 # print('[+] Puerto abierto '+ str(port))
 
 targets_ip =  input('Escriba el dominio o ips a escanear (separados por coma): ')
-port_num = input('Ingrese la cantidad de puertos que quiere scanear: ')
+port_num = int(input('Ingrese la cantidad de puertos que quiere scanear: '))
 # vuln_file = input(['Ingrese direccion a carpeta con sw vulnerables])
-"""
+
 if ',' in targets_ip:
             for ip_add in targets_ip.split(','):
                 #scan(ip_add.strip(' '),port_num)
@@ -15,8 +16,30 @@ if ',' in targets_ip:
         else:
             #scan(targets,port_num)
             scan(targets)
-"""
+
 print('\n')
 target = PortScanner.PortScan(targets_ip, port_num)
 target.scan()
+
+"""
+import PortScanner
+
+targets_ip = input('[+] * Enter Target To Scan For Vulnerable Open Ports: ')
+port_number = int(input('[+] * Enter Amount Of Ports You Want To Scan(500 - first 500 ports): '))
+vul_file = input('[+] * Enter Path To The File With Vulnerable Softwares: ')
+print('\n')
+
+target = PortScanner.PortScan(targets_ip, port_number)
+target.scan()
+
+with open(vul_file, 'r') as file:
+    count = 0
+
+    for banner in target.banners:
+        file.seek(0)
+        for line in file.readlines():
+            if line.strip() in banner:
+                print('[!!] VULNERABLE BANNER: "' + banner + '" ON PORT: ' + str(target.open_ports[count]))
+        count += 1
+        
 
