6118c3c6441416d43b8a2eb67ca931a402127070
LeandroGarza <leandrogarzap@gmail.com>
2024-07-12 14:48:39 -0300

    mejoras esteticas

--- Fussion/fussion_multitool.py
+++ Fussion/fussion_multitool.py
@@ -45,6 +45,7 @@ def is_vulnerable(product, version):
 def scan(target):
     nm = nmap.PortScanner()
     nm.scan(hosts=target, arguments='-p 20,21,22,25,53,80,110,123,143,179,443,465,500,587,993,995,2222,3389,41648 -sV -sC')  # Escaneo de todos los puertos con detección de versiones
+    vulnerabilities_found = False
     
     for host in nm.all_hosts():
         print('\n' + 'Escaneando el objetivo ' + str(host))
@@ -56,86 +57,26 @@ def scan(target):
                 version = nm[host][proto][port].get('version', '')
                 if product and version:
                     product_version = f"{product} {version}"
-                    print('[+] Puerto abierto ' + str(port) + ' : ' + product_version)
+                    print('[+] El puerto abierto ' + str(port) + ' esta corriendo el servicio ' + product_version)
                     is_vuln, vul_data = is_vulnerable(product, version)
                     if is_vuln:
-                        print(termcolor.colored(f'[!!] VULNERABLE BANNER: "{product_version}" ON PORT: {str(port)}', 'red'))
+                        vulnerabilities_found = True
+                        print(termcolor.colored(f'Se recomienda actualizar version: "{product_version}" en puerto: {str(port)}', 'red'))
                         print('Detalles de la vulnerabilidad:')
                         print(f"  Título: {vul_data.get('title', 'No disponible')}")
                         print(f"  CVSS Score: {vul_data.get('cvss_score', 'No disponible')}")
                         print(f"  Descripción: {vul_data.get('description', 'No disponible')}")
                         print(f"  Referencias: {vul_data.get('references', 'No disponible')}\n")
-
+                        
+    if not vulnerabilities_found:
+        print(termcolor.colored("¡Felicitaciones, no se han encontrado versiones vulnerables en su sistema!", "green"))
+           
 targets = input('Escriba el dominio o ip a escanear: ')
 if ',' in targets:
     for ip_add in targets.split(','):
         scan(ip_add.strip(' '))
 else:
     scan(targets)
-
-
-"""
-def is_vulnerable(product, version):
-    api_url = "https://vulners.com/api/v3/search/lucene/"
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Vulners-ApiKey': API_KEY
-    }
-    query = f"{product} {version}"
-    data = {
-        "query": query,
-        "size": 1  # Limita el tamaño de la respuesta
-    }
-    
-    try:
-        response = requests.post(api_url, headers=headers, json=data)
-        response.raise_for_status()  # Lanza un error si la respuesta HTTP es un error
-        data = response.json()
-        if data.get('data', {}).get('total', 0) > 0:
-            return True, data['data']['search'][0]
-        else:
-            return False, {}
-    except requests.exceptions.RequestException as e:
-        print(f"Error al consultar la API de Vulners: {e}")
-        return False, {}
-    except ValueError as e:
-        print(f"Error al decodificar la respuesta JSON: {e}")
-        return False, {}
-
-def scan(target):
-    nm = nmap.PortScanner()
-    nm.scan(hosts=target, arguments='-p 20,21,22,25,53,80,110,123,143,179,443,465,500,587,993,995,2222,3389,41648 -sV -sC')  # Escaneo de todos los puertos con detección de versiones
-    
-    for host in nm.all_hosts():
-        print('\n' + 'Escaneando el objetivo ' + str(host))
-        for proto in nm[host].all_protocols():
-            print('Protocolo : %s' % proto)
-            ports = nm[host][proto].keys()
-            for port in ports:
-                product = nm[host][proto][port].get('product', '')
-                version = nm[host][proto][port].get('version', '')
-                if product and version:
-                    product_version = f"{product} {version}"
-                    print('[+] Puerto abierto ' + str(port) + ' : ' + product_version)
-                    is_vuln, vul_data = is_vulnerable(product, version)
-                    if is_vuln:
-                        print(termcolor.colored(f'[!!] VULNERABLE BANNER: "{product_version}" ON PORT: {str(port)}', 'red'))
-                        print('Detalles de la vulnerabilidad:')
-                        print(f"  Título: {vul_data.get('title', 'No disponible')}")
-                        print(f"  CVSS Score: {vul_data.get('cvss', {}).get('score', 'No disponible')}")
-                        print(f"  Descripción: {vul_data.get('description', 'No disponible')}")
-                        print(f"  Referencias: {vul_data.get('vhref', 'No disponible')}\n")
-
-targets = input('Escriba el dominio o ip a escanear: ')
-if ',' in targets:
-    for ip_add in targets.split(','):
-        scan(ip_add.strip(' '))
-else:
-    scan(targets)
-"""
-
-
-
 
 # escaneo de puertos en base a txt
 """
@@ -179,9 +120,9 @@ def ssh_connect(username, password):
     try:
         ssh.connect(host, port=22, username=username, password=password)
         stop_flag = True
-        print(termcolor.colored(('[+] Found Password: ' + password + ', For Account: ' + username), 'green'))
+        print(termcolor.colored(('[+] Found Password: ' + password + ', For User: ' + username), 'green'))
     except paramiko.ssh_exception.AuthenticationException:
-        print(termcolor.colored(('[-] Incorrect Password: ' + password + ', For Account: ' + username), 'red'))
+        print(termcolor.colored(('[-] Incorrect Password: ' + password + ', For User: ' + username), 'red'))
     except paramiko.ssh_exception.SSHException as e:
         print(termcolor.colored(('[-] SSH Exception: ' + str(e)), 'red'))
     except Exception as e:
@@ -199,7 +140,7 @@ if os.path.exists(passwords_file) == False:
     print('[!!] That File/Path Doesnt Exist')
     sys.exit(1)
 
-print('Empezando fuerza bruta en host ' + host + ' con el usuario: ' + usernames_file ) 
+print('Empezando fuerza bruta en host ' + host ) 
 
 with open(usernames_file, 'r') as users:
     for username in users:
@@ -212,7 +153,7 @@ with open(usernames_file, 'r') as users:
                 thread_limiter.acquire()
                 t = threading.Thread(target=ssh_connect, args=(username, password))
                 t.start()
-                time.sleep(0.5)  # Ajusta el tiempo de espera según sea necesario
+                time.sleep(0.5)
                 if stop_flag:
                     break
         if stop_flag:
