88c474faabb1d8943fa39df7008c217ebe90a9cb
LeandroGarza <leandrogarzap@gmail.com>
2024-07-25 16:50:15 -0300

    se esconde introduccion al clickear en comenzar

--- Fussion/FrontEnd/static/js/script.js
+++ Fussion/FrontEnd/static/js/script.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('scan-form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        document.getElementById('tasks').style.display = 'none';
         document.getElementById('message').innerText = 'Comenzando el escaneo. Esto suele tardar de 2 a 10 minutos.';
         const target = document.getElementById('target').value;
-        fetch('http://127.0.0.1:5000/scan', {  // URL correcta
+        fetch('http://127.0.0.1:5000/scan', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
