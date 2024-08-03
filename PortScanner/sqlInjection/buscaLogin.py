065b177dc870c233e7108326483c790388309eb9
LeandroGarza <leandrogarzap@gmail.com>
2024-08-03 14:26:38 -0300

    mayor flexibilidad al poner url

--- PortScanner/sqlInjection/buscaLogin.py
+++ PortScanner/sqlInjection/buscaLogin.py
@@ -2,22 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 
 def find_login_url(targets):
-    common_login_paths = ['/login', '/signin', '/auth', '/user/login']
+    common_login_paths = [
+        '/login',
+        '/signin',
+        '/auth',
+        '/user/login',
+        '/users/login',
+        '/account/login',
+        '/accounts/login',
+        '/admin/login',
+        '/access/login',
+        '/session/login',
+        '/login.php',
+        '/login.html',
+        '/auth/login',
+        '/authenticate',
+        '/member/login'
+    ]
     
+    if not targets.startswith(('http://', 'https://')):
+        targets = 'http://' + targets  # O 'https://' si prefieres HTTPS por defecto
+
     for path in common_login_paths:
-        url = targets + path
-        response = requests.get(url)
-        
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+        url = targets.rstrip('/') + path
+        try:
+            response = requests.get(url)
             
-            # Buscar un formulario de login
-            forms = soup.find_all('form')
-            for form in forms:
-                if form.find('input', {'type': 'password'}) and (form.find('input', {'type': 'text'}) or form.find('input', {'type': 'email'})):
-                    print(f"Login page found at: {url}")
-                    return url
-    
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                
+                # Buscar un formulario de login
+                forms = soup.find_all('form')
+                for form in forms:
+                    if form.find('input', {'type': 'password'}) and (form.find('input', {'type': 'text'}) or form.find('input', {'type': 'email'})):
+                        print(f"Login page found at: {url}")
+                        return url
+        except requests.RequestException as e:
+            print(f"Error al acceder a {url}: {e}")
+
     print("No login page found.")
     return None
 
@@ -27,3 +49,4 @@ if __name__ == "__main__":
     if login_url:
         # inyección SQL en login_url
         pass
+
